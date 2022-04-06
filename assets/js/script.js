089d0fbd19f09f34aa421921134eca76c039b8a2
pasindulakshankudaligama <kudaligamagepasindulakshan@gmail.com>
2022-04-06 13:02:59 +0530

    move the cars & lines

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-$(function() {
+$(function () {
 
     var anim_id;
 
@@ -37,68 +37,140 @@ $(function() {
     var move_up = false;
     var move_down = false;
 
-/* Move the cars */
-$(document).on('keydown', function(e) {
-    if (game_over === false) {
-        var key = e.keyCode;
-        if (key === 37 && move_left === false) {
+    /* Move the cars */
+    $(document).on('keydown', function (e) {
+        if (game_over === false) {
+            var key = e.keyCode;
+            if (key === 37 && move_left === false) {
+                move_left = requestAnimationFrame(left);
+            } else if (key === 39 && move_right === false) {
+                move_right = requestAnimationFrame(right);
+            } else if (key === 38 && move_up === false) {
+                move_up = requestAnimationFrame(up);
+            } else if (key === 40 && move_down === false) {
+                move_down = requestAnimationFrame(down);
+            }
+        }
+    });
+
+    $(document).on('keyup', function (e) {
+        if (game_over === false) {
+            var key = e.keyCode;
+            if (key === 37) {
+                cancelAnimationFrame(move_left);
+                move_left = false;
+            } else if (key === 39) {
+                cancelAnimationFrame(move_right);
+                move_right = false;
+            } else if (key === 38) {
+                cancelAnimationFrame(move_up);
+                move_up = false;
+            } else if (key === 40) {
+                cancelAnimationFrame(move_down);
+                move_down = false;
+            }
+        }
+    });
+
+    function left() {
+        if (game_over === false && parseInt(car.css('left')) > 0) {
+            car.css('left', parseInt(car.css('left')) - 5);
             move_left = requestAnimationFrame(left);
-        } else if (key === 39 && move_right === false) {
+        }
+    }
+
+    function right() {
+        if (game_over === false && parseInt(car.css('left')) < container_width - car_width) {
+            car.css('left', parseInt(car.css('left')) + 5);
             move_right = requestAnimationFrame(right);
-        } else if (key === 38 && move_up === false) {
+        }
+    }
+
+    function up() {
+        if (game_over === false && parseInt(car.css('top')) > 0) {
+            car.css('top', parseInt(car.css('top')) - 3);
             move_up = requestAnimationFrame(up);
-        } else if (key === 40 && move_down === false) {
+        }
+    }
+
+    function down() {
+        if (game_over === false && parseInt(car.css('top')) < container_height - car_height) {
+            car.css('top', parseInt(car.css('top')) + 3);
             move_down = requestAnimationFrame(down);
         }
     }
-});
 
-$(document).on('keyup', function(e) {
-    if (game_over === false) {
-        var key = e.keyCode;
-        if (key === 37) {
-            cancelAnimationFrame(move_left);
-            move_left = false;
-        } else if (key === 39) {
-            cancelAnimationFrame(move_right);
-            move_right = false;
-        } else if (key === 38) {
-            cancelAnimationFrame(move_up);
-            move_up = false;
-        } else if (key === 40) {
-            cancelAnimationFrame(move_down);
-            move_down = false;
+    /* Move the cars and lines */
+    anim_id = requestAnimationFrame(repeat);
+
+    function repeat() {
+        if (collision(car, car_1) || collision(car, car_2) || collision(car, car_3)) {
+            stop_the_game();
+            return;
         }
-    }
-});
 
-function left() {
-    if (game_over === false && parseInt(car.css('left')) > 0) {
-        car.css('left', parseInt(car.css('left')) - 5);
-        move_left = requestAnimationFrame(left);
-    }
-}
+        score_counter++;
 
-function right() {
-    if (game_over === false && parseInt(car.css('left')) < container_width - car_width) {
-        car.css('left', parseInt(car.css('left')) + 5);
-        move_right = requestAnimationFrame(right);
-    }
-}
+        if (score_counter % 20 == 0) {
+            score.text(parseInt(score.text()) + 1);
+        }
+        if (score_counter % 500 == 0) {
+            speed++;
+            line_speed++;
+        }
 
-function up() {
-    if (game_over === false && parseInt(car.css('top')) > 0) {
-        car.css('top', parseInt(car.css('top')) - 3);
-        move_up = requestAnimationFrame(up);
-    }
-}
+        car_down(car_1);
+        car_down(car_2);
+        car_down(car_3);
 
-function down() {
-    if (game_over === false && parseInt(car.css('top')) < container_height - car_height) {
-        car.css('top', parseInt(car.css('top')) + 3);
-        move_down = requestAnimationFrame(down);
+        line_down(line_1);
+        line_down(line_2);
+        line_down(line_3);
+
+        anim_id = requestAnimationFrame(repeat);
     }
-}
+
+    function car_down(car) {
+        var car_current_top = parseInt(car.css('top'));
+        if (car_current_top > container_height) {
+            car_current_top = -200;
+            var car_left = parseInt(Math.random() * (container_width - car_width));
+            car.css('left', car_left);
+        }
+        car.css('top', car_current_top + speed);
+    }
+
+    function line_down(line) {
+        var line_current_top = parseInt(line.css('top'));
+        if (line_current_top > container_height) {
+            line_current_top = -300;
+        }
+        line.css('top', line_current_top + line_speed);
+    }
+
+    restart_btn.click(function() {
+        location.reload();
+    });
+
+    function stop_the_game() {
+        game_over = true;
+        cancelAnimationFrame(anim_id);
+        cancelAnimationFrame(move_right);
+        cancelAnimationFrame(move_left);
+        cancelAnimationFrame(move_up);
+        cancelAnimationFrame(move_down);
+        restart_div.slideDown();
+        restart_btn.focus();
+        setHighScore();
+    }
+
+    function setHighScore() {
+        if (high_score < parseInt(score.text())) {
+            high_score = parseInt(score.text());
+            localStorage.setItem('high_score', parseInt(score.text()));
+        }
+        $('#high_score').text(high_score);
+    }
 
 
 
